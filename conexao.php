cae20d680674936fbafe4d0cae3c01dcf4c6f97a
sistema2 <coringajoker0428@gmail.com>
2019-10-23 16:23:22 -0300

    sistema2

---
+++ conexao.php
@@ -0,0 +1,19 @@
+<?php
+class Conexao{
+    
+    private function __construct(){
+        
+    }
+    public static function getInstance(){
+        try {
+            $conexao = new PDO("mysql:host=localhost; dbname=test", "root", "");
+            $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conexao->exec("set names utf8");
+            
+            return $conexao;
+        } catch (PDOException $erro) {           
+            return null;
+        }
+    }
+}
+?>
