9387dc33ee99a4348716451fb1a1ca68af6d0dd5
sistema2 <coringajoker0428@gmail.com>
2019-10-23 15:49:25 -0300

    sistema2

--- cliente.class.php
+++ cliente.class.php
@@ -6,7 +6,20 @@
      public $email;
      public $cpf;
 
+     public function setEmail($email){
+        $this->email = $email;
+     }
+     public function getEmail(){
+        return $this->email;
+     }
+     
     }
+
+    $obj = new Cliente ();
+    $obj-> setEmail("caio@email.com");// com encapsulamento
+    $obj->email = "caio@email.com";// sem encapsulamento
+
+    
 
 
 ?>
