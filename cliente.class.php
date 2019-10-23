52448e21febc9bf734e2b97833853b7541430557
sistema2 <coringajoker0428@gmail.com>
2019-10-23 16:05:11 -0300

    sistema2

--- cliente.class.php
+++ cliente.class.php
@@ -3,21 +3,30 @@
      public $id;
      public $nome;
      public $telefone;
-     public $email;
+    
      public $cpf;
+    
+     private $email;
 
-     public function setEmail($email){
-        $this->email = $email;
-     }
-     public function getEmail(){
-        return $this->email;
-     }
+        public function setEmail($email){
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+             $this->email = $email;
+            }else{
+                throw new Expection('E-mail Inválido');
+            } 
+        }
+        public function getEmail(){
+          return $this->email;
+        }
      
     }
 
     $obj = new Cliente ();
     $obj-> setEmail("caio@email.com");// com encapsulamento
+    echo $obj->getEmail();// com encapsulamento
+    echo "<br/>";
     $obj->email = "caio@email.com";// sem encapsulamento
+    echo $obj->email;// sem encapsulamento
 
     
 
